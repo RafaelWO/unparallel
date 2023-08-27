1b72f33c8c1894832a37fba62b1de18ba0d377ea
rafaelweingartner <weingartner@symptoma.com>
2023-08-27 11:05:55 +0200

    :tada: Initial commit

---
+++ unparallel/__init__.py
@@ -0,0 +1,15 @@
+# type: ignore[attr-defined]
+"""Create async web requests in no time with `unparallel`"""
+
+import sys
+from importlib import metadata as importlib_metadata
+
+
+def get_version() -> str:
+    try:
+        return importlib_metadata.version(__name__)
+    except importlib_metadata.PackageNotFoundError:  # pragma: no cover
+        return "unknown"
+
+
+version: str = get_version()
