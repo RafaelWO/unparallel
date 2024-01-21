1f759ceffcdaf01046ed9cb3b10c9a2fb7f10eb6
RafaelWO <weingartner.rafael@hotmail.com>
2024-01-21 19:02:32 +0100

    ♻️ Refactor example for mulitple websites

--- docs/examples/multiple_websites.py
+++ docs/examples/multiple_websites.py
@@ -3,70 +3,69 @@ This script is based on a use case at StackOverflow and fetches the content of v
 webpages.
 
 See Also:
-    https://stackoverflow.com/a/57129241
+    https://www.stackoverflow.com/a/57129241
 """
 import asyncio
-from pprint import pp
-
-import httpx
 
 from unparallel import up
 
-websites = """https://google.com/
-https://youtube.com/
-https://facebook.com/
-https://twitter.com/
-https://wikipedia.org/
-https://instagram.com/
-https://reddit.com/
-https://amazon.com/
-https://duckduckgo.com/
-https://yahoo.com/
-https://tiktok.com/
-https://bing.com/
-https://yahoo.co.jp/
-https://weather.com/
-https://whatsapp.com/
-https://yandex.ru/
-https://openai.com/
-https://live.com/
-https://microsoft.com/
-https://microsoftonline.com/
-https://linkedin.com/
-https://quora.com/
-https://twitch.tv/
-https://naver.com/
-https://netflix.com/
-https://office.com/
-https://vk.com/
-https://globo.com/
-https://Aliexpress.com/
-https://cnn.com/
-https://zoom.us/
-https://imdb.com/
-https://x.com/
-https://newyorktimes.com/
-https://onlyfans.com/
-https://espn.com/
-https://amazon.co.jp/
-https://pinterest.com/
-https://uol.com.br/
-https://ebay.com/
-https://marca.com/
-https://canva.com/
-https://spotify.com/
-https://bbc.com/
-https://paypal.com/
-https://apple.com/"""
+websites = """https://www.google.com/
+https://www.youtube.com/
+https://www.facebook.com/
+https://www.wikipedia.org/
+https://www.instagram.com/
+https://www.reddit.com/
+https://www.amazon.com/
+https://www.duckduckgo.com/
+https://www.yahoo.com/
+https://www.tiktok.com/
+https://www.bing.com/
+https://www.yahoo.co.jp/
+https://www.weather.com/
+https://www.whatsapp.com/
+https://www.yandex.ru/
+https://www.openai.com/
+https://www.live.com/
+https://www.microsoft.com/
+https://www.linkedin.com/
+https://www.quora.com/
+https://www.twitch.tv/
+https://www.naver.com/
+https://www.netflix.com/
+https://www.office.com/
+https://www.vk.com/
+https://www.globo.com/
+https://www.Aliexpress.com/
+https://www.cnn.com/
+https://www.zoom.us/
+https://www.imdb.com/
+https://www.x.com/
+https://www.nytimes.com/
+https://www.onlyfans.com/
+https://www.espn.com/
+https://www.amazon.co.jp/
+https://www.pinterest.com/
+https://www.uol.com.br/
+https://www.ebay.com/
+https://www.marca.com/
+https://www.canva.com/
+https://www.spotify.com/
+https://www.bbc.com/
+https://www.paypal.com/
+https://www.apple.com/"""
 
 
 async def main():
     urls = websites.split("\n")
 
     # Get all pages
-    results = await up(urls, method="GET", response_fn=lambda x: x.text)
+    results = await up(
+        urls, method="GET", response_fn=lambda x: x.text, raise_for_status=False
+    )
+
+    # Print the content of the first 5 pages
     for url, content in zip(urls, results[:5]):
-        print(url, content[:100])
+        print(url, repr(content[:100]))
 
 
 results = asyncio.run(main())
