45837270b8502620f6a37f9d3b3c40894cf124ad
RafaelWO <weingartner.rafael@hotmail.com>
2024-04-01 16:09:06 +0200

    🏷️ Use mypy strict

--- unparallel/utils.py
+++ unparallel/utils.py
@@ -1,9 +1,15 @@
+from types import TracebackType
+from typing import Any, Type
+
+
 class AsyncNullContext:
     """A nullcontext including __aenter__ and __aexit__ to support Python versions
     below 3.10."""
 
-    async def __aenter__(self):
+    async def __aenter__(self) -> None:
         return None
 
-    async def __aexit__(self, *excinfo):
+    async def __aexit__(
+        self, exc_type: Type[Exception], exc_value: Any, traceback: TracebackType
+    ) -> None:
         pass
