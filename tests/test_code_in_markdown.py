cc2b73125fd015e24d15d41ca652c4198a52dc1b
RafaelWO <weingartner.rafael@hotmail.com>
2024-04-14 11:45:16 +0200

    ✅ Mock hosts in tests and limit max requests in example tests

--- tests/test_code_in_markdown.py
+++ tests/test_code_in_markdown.py
@@ -1,8 +1,10 @@
+import re
+
 import pytest
 from pytest_examples import CodeExample, EvalExample, find_examples
 
 
-def skip_check(example: CodeExample):
+def check_for_skip(example: CodeExample):
     """Checks whether a code block is declared to be skipped via a comment above."""
     with open(example.path) as file:
         for i, line in enumerate(file):
@@ -11,16 +13,24 @@ def skip_check(example: CodeExample):
                     pytest.skip("Found 'skip-test' above source code")
 
 
+def limit_requests(example: CodeExample, num_requests: int = 3):
+    """Change the example's source code to only make `num_requests` requests."""
+    example.source = re.sub(
+        r"range\([\d, ]+\)", f"range({num_requests})", example.source
+    )
+
+
 @pytest.mark.parametrize("example", find_examples("README.md"), ids=str)
 def test_readme(example: CodeExample, eval_example: EvalExample):
-    skip_check(example)
+    check_for_skip(example)
+    limit_requests(example)
     eval_example.lint(example)
     eval_example.run(example)
 
 
 @pytest.mark.parametrize("example", find_examples("docs/usage.md"), ids=str)
 def test_docs_usage(example: CodeExample, eval_example: EvalExample):
-    find_examples("docs/examples/")
-    skip_check(example)
+    check_for_skip(example)
+    limit_requests(example)
     eval_example.lint(example)
     eval_example.run(example)
